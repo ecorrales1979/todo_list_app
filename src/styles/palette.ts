e228bea9ced5d4292516acdb267f9b13d9478694
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-05 22:35:06 -0300

    style: add definitions of color palette

---
+++ src/styles/palette.ts
@@ -0,0 +1,14 @@
+export const palette: Record<string, string> = {
+  blue: '#4EA8DE',
+  blueDark: '#1E6F9F',
+  purple: '#8284FA',
+  purpleDark: '#5E60CE',
+  gray1: '#F2F2F2',
+  gray2: '#D9D9D9',
+  gray3: '#808080',
+  gray4: '#333333',
+  gray5: '#262626',
+  gray6: '#1A1A1A',
+  gray7: '#0D0D0D',
+  danger: '#E25858',
+};
