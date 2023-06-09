97290bb4e72b9d3f095f58baedc0775028d89e54
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-08 22:10:50 -0300

    chore: add inter font

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Inter_400Regular as InterRegular,
+  Inter_700Bold as InterBold
+} from '@expo-google-fonts/inter';
 
 import Home from '@/screens/home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ InterRegular, InterBold })
+
+  if (!fontsLoaded) return null;
+
   return (
     <>
       <Home />
-      <StatusBar style="light" translucent  />
+      <StatusBar style="light" translucent />
     </>
   );
 }
